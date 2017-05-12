4015599da61d2b69dd616d64b5d164da0c2371f5
Owen Duignan <owenied@me.com>
2017-05-12 06:44:06 -0500

    first commit

---
+++ NamestoFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NamestoFaces
+//
+//  Created by Owen Duignan on 12/05/2017.
+//  Copyright © 2017 Owen Duignan. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
